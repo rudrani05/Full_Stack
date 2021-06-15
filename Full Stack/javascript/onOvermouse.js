06127c0e257b9497e762910b045d530962995dec
Rudrani <60399242+rudrani05@users.noreply.github.com>
2021-06-15 19:51:53 +0530

    Add files via upload

---
+++ Full Stack/javascript/onOvermouse.js
@@ -0,0 +1,12 @@
+function changeDivOne(){
+    document.getElementById("divone").style.backgroundColor="brown";
+}
+
+function changeDivTwo(){
+    document.getElementById("divtwo").style.backgroundColor="blue";
+    document.body.style.backgroundColor="pink";
+}
+
+function changeText(){
+    document.getElementById("para").innerHTML="fuck... you have changed all the text";
+}
