3f9d78b0df5d42a7c51bdc6be55e0f4d6a935a2f
Rudrani <60399242+rudrani05@users.noreply.github.com>
2021-06-15 19:16:54 +0530

    Add files via upload

---
+++ Full Stack/javascript/changingCss.js
@@ -0,0 +1,6 @@
+function changeCss(){
+    document.getElementById("boringP").innerHTML="working";
+    document.getElementById("boringP").style.backgroundColor="red";
+    document.getElementById("boringP").style.border="3px solid green"
+    document.body.style.backgroundColor="pink";
+}
