80f255d243793a3b87aedeeb70560bd7c9a90b65
Rudrani <60399242+rudrani05@users.noreply.github.com>
2021-06-16 19:45:37 +0530

    Add files via upload

---
+++ Full Stack/javascript/dataTypes.js
@@ -0,0 +1,10 @@
+var num=50;
+var mystring="Hi there";
+var mySecondstring=".how you doing?";
+var array=["apple",900,"fuck off","bitch"];
+var myBool=True;
+var myObjects={'Rini':22,'Jou':100,'Raj':56};
+
+function myAlert(myvar){
+    alert(myvar);
+}
