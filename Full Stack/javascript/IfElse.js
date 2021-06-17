88ba9e430b288bf4c04e51983f210bf1af40c1c6
Rudrani <60399242+rudrani05@users.noreply.github.com>
2021-06-17 13:55:57 +0530

    Add files via upload

---
+++ Full Stack/javascript/IfElse.js
@@ -0,0 +1,12 @@
+function ifElse(){
+   var cp="myp1234"
+   var ap="paass?"
+    if (cp==ap){
+        alert("password is correct-access garnted!!")
+    }
+    else{
+        alert("password is incorrect!-try again you have only 3 more attempt left!!")
+    }
+
+   
+}
